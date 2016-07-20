304a242350676f049365b6beb4dde84d0d55bc21
leocoder <ngixpro@qq.com>
2016-07-20 20:27:13 +0800

    1 add pyftpdlib libs 2 add auto setup files 3 modify FtpConfig.ini section

--- FtpServer/PyFtpModule.py
+++ FtpServer/PyFtpModule.py
@@ -4,17 +4,15 @@
 import ConfigParser
 import os, sys
 import ctypes
-
 sys.path.append(os.path.abspath('..'))
-
-
-
-
 from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import ThreadedFTPServer
-
 from Logger import  logger
+import platform
+
+
+
 
 
 g_user =u"ITC"
@@ -25,11 +23,9 @@ g_banonymous = False
 g_hided = False
 
 # advanced feature
-g_banner =u"welcome to ITC smart_edu ftp for test"
-g_listen_ip = u"0.0.0.0"
 g_max_connect=256
 g_permission = "elradfmw"                #允许权限
-g_bserver = 0                    #是否注册为系统服务
+g_bautosetup = 0                         #是否随机启动
 
 
 
@@ -39,35 +35,65 @@ def read_config():
     global g_dir
     global g_port
     global g_banonymous
-    global g_banner
-    global g_listen_ip
+    global g_hided
+
+    global g_bautosetup
     global g_max_connect
     global g_permission
-    global g_bserver
-    global g_hided
+
     try:
         cf = ConfigParser.ConfigParser()
         cf.read(os.path.join(os.getcwd(), "FtpConfig.ini"))
-        g_user = cf.get("base", "user").decode("GBK")
-        g_password = cf.get("base", "password").decode("GBK")
-        g_dir = cf.get("base", "dir", sys.path[0]).decode("GBK")
-        g_port = cf.getint("base", "port")
-        g_banonymous = cf.getboolean("base", "anonymous")
-        g_hided = cf.getboolean("base" , "hided")
-        return True
 
-        # read advanced feature
-        # g_listen_ip = cf.get("advanced","listen_ip" ,"0.0.0.0")
-        # g_max_connect = cf.get("advanced","max_connect" ,256)
-        # g_permission = cf.get("advanced","permission","elradfmw")
-        # g_bserver = cf.get("advanced","bserver",0)
-        #
+        if cf.has_option("base","user"):
+            g_user = cf.get("base", "user").decode("GBK")
+        if cf.has_option("base","password"):
+            g_password = cf.get("base", "password").decode("GBK")
+        if cf.has_option("base","dir"):
+            g_dir = cf.get("base", "dir", sys.path[0]).decode("GBK")
+        if cf.has_option("base","port"):
+            g_port = cf.getint("base", "port")
+        if cf.has_option("base","anonymous"):
+            g_banonymous = cf.getboolean("base", "anonymous")
+        if cf.has_option("base","hided"):
+            g_hided = cf.getboolean("base" , "hided")
+        #////////////////////////////////high level feature
+        if cf.has_option("advanced","max_connect"):
+            g_max_connect = cf.getint("advanced","max_connect" )
+        if cf.has_option("advanced","permission"):
+            g_permission = cf.get("advanced","permission","elradfmw").decode("GBK")
+        if cf.has_option("advanced","autosetup"):
+            g_bautosetup = cf.getboolean("advanced","autosetup")
+
+        check_ini_para()
+        return  True
     except Exception, ex:
         print ex
         logger.error(ex)
         os.system("pause")
         return False
 
+
+def check_ini_para():
+    global g_user
+    global g_password
+    global g_dir
+    global g_port
+    global g_banonymous
+    global g_hided
+    global g_bautosetup
+    global g_max_connect
+    global g_permission
+
+    if len(g_permission) == 0:
+        g_permission = "elradfmw"
+        logger.warning("permission error")
+    if g_max_connect < 1:
+        g_max_connect =1
+        logger.warning("the allowed connecting could not smaller than 1")
+    if os.path.exists(g_dir) is False:
+        os.makedirs(g_dir)
+        logger.info("create dir : " + g_dir)
 
 
 
@@ -78,11 +104,14 @@ def run():
     global g_dir
     global g_port
     global g_banonymous
-    global g_banner
-    global g_listen_ip
+    global g_hided
+    global g_bautosetup
     global g_max_connect
     global g_permission
-    global g_bserver
+
+    logger.info("currnt dir : " +g_dir)
+    print ("currnt dir : " +g_dir)
+
     try:
         authorizer = DummyAuthorizer()
         if g_banonymous:
@@ -91,8 +120,7 @@ def run():
             authorizer.add_user(g_user, g_password, g_dir, perm=g_permission)
         handler = FTPHandler
         handler.authorizer = authorizer
-        handler.banner = g_banner
-        server = ThreadedFTPServer((g_listen_ip, g_port), handler)
+        server = ThreadedFTPServer(("0.0.0.0", g_port), handler)
         server.max_cons = g_max_connect
         server.serve_forever()
 
@@ -105,22 +133,28 @@ def run():
 
 
 def hide_wnd():
+    if platform.system() != "Windows":
+        return
     global  g_hided
     if g_hided is False:
         logger.info("windoes not hided")
         return
-
     whnd = ctypes.windll.kernel32.GetConsoleWindow()
     if whnd != 0:
         ctypes.windll.user32.ShowWindow(whnd, 0)
         logger.debug("windows hided")
         # ctypes.windll.kernel32.CloseHandle(whnd)
 
+def auto_setup(t_bsetup):
+    if t_bsetup is False:
+        return
+    # doing something to setup with windows
+
 
 
 if __name__ == "__main__":
-    hide_wnd()
     if read_config():
+        hide_wnd()
         run()
 
 
